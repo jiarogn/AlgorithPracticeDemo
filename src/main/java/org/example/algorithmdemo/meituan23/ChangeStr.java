8e13c19f372e1000562e208dfd632ede6c2284fb
jiarong <sxyang@iflytek.com>
2024-07-31 18:09:50 +0800

    feat:小小美的蛋糕切割

--- src/main/java/org/example/algorithmdemo/meituan23/ChangeStr.java
+++ src/main/java/org/example/algorithmdemo/meituan23/ChangeStr.java
@@ -1,4 +1,18 @@
 package org.example.algorithmdemo.meituan23;
 
+import java.util.Scanner;
+
+//131. 小美的字符串变换
 public class ChangeStr {
+
+    /*
+    *  x * y=n 有这么个东西，让我想直接开始求因数好了
+    *
+    * */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String str = sc.next();
+
+    }
 }
